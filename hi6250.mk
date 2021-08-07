7441caf1f2f22dbb787f3c5ff12ab9c7efe022c1
Demon Singur <demonsingur@gmail.com>
2021-08-07 15:51:12 +0200

    hi6250-common: Build separate system and vendor hidl libs
    Change-Id: I0bdbfc51b0a54606120b582320d4ddf22e0a11ef

--- hi6250.mk
+++ hi6250.mk
@@ -13,7 +13,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system
 
 # Ramdisk's Charger
 PRODUCT_PACKAGES += \
