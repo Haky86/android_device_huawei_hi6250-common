719479c360141eafcb456538cb6afa757af28e77
Haky86 <hakamelassouad@gmail.com>
2021-08-07 03:35:58 +0200

    hi6250-common: Setup arch
    Change-Id: I653f06f95b1bdc9a4709ed67d7ec4d029094f2be

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,4 +6,29 @@
 
 COMMON_PATH := device/huawei/hi6250-common
 
+# 64 Bit
+TARGET_USES_64_BIT_BINDER := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
+# CPU
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
 include vendor/huawei/hi6250-common/BoardConfigVendor.mk
